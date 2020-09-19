99beb5f15d5ae239084e46adf809729e506de64a
saar cohen <saar.cohenn@gmail.com>
2020-09-19 18:02:12 +0300

    finish creating Header Component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBNSz5robD7SGG0t3guMUPzQqn98wQG_jw",
+    authDomain: "sc-clone-b9258.firebaseapp.com",
+    databaseURL: "https://sc-clone-b9258.firebaseio.com",
+    projectId: "sc-clone-b9258",
+    storageBucket: "sc-clone-b9258.appspot.com",
+    messagingSenderId: "663635577243",
+    appId: "1:663635577243:web:0977d158fc92ff9974393e"
+};
